8b801bc55ddc82ed6c9e119f6656d5911c1689e9
3cvms3 <cvms333@yahoo.com>
2024-11-26 16:13:46 +0100

    NoBrelan: test

--- index.test.js
+++ index.test.js
@@ -5,4 +5,8 @@ describe('Yams', () => {
     const result = getBrelan([2, 2, 2, 4, 5]);
     expect(result).toBe(28);
   });
+  test('No Brelan', () => {
+    const result = getBrelan([1, 2, 3, 4, 5]);
+    expect(result).toBe(0);
+  });
 });
